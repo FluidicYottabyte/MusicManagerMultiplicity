d243542c7e82ca8f1776330f7b9d35abb3826685
FluidicYottabyte <70356834+FluidicYottabyte@users.noreply.github.com>
2026-07-20 19:24:08 -0700

    feat: Add Swift Package Manager configuration for Vapor backend

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "MusicManager",
+    platforms: [
+        .macOS(.v13)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.89.0"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.4.0"),
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.3.0")
+    ],
+    targets: [
+        .executableTarget(
+            name: "App",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver")
+            ],
+            path: "Sources/App"
+        )
+    ]
+)
